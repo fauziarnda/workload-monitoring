af161adf8c903b73b445779e88b3ceec5848a7da
fauziarnda <fauzi.aranda@gmail.com>
2025-09-29 19:22:57 +0700

    setup db supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { supabase } from '@/lib/supabase/client';
 import { Button } from '@/components/ui/button';
 
-export default function Home() {
+export default async function Home() {
+  const { data: employees } = await supabase.from('employees').select('*');
+  console.log(employees);
+
   return (
     <div className="flex justify-center place-items-center">
       <Button>Hello World</Button>
